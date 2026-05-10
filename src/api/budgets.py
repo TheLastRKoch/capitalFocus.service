1de037b327b859e03246fb571b4e2e52298c73c0
Sergio Segura <thelastrkoch@gmail.com>
2026-05-10 11:58:21 -0600

    Fixed budget details UI

--- src/api/budgets.py
+++ src/api/budgets.py
@@ -12,28 +12,30 @@ bp = Blueprint(prefix, __name__, url_prefix=f"/{prefix}")
 teable_service = TeableService(Config.TEABLE_API_TOKEN, Config.TEABLE_URL)
 budgets_repo = BudgetsRepository(teable_service, Config.TEABLE_BUDGETS)
 sections_repo = SectionsRepository(teable_service, Config.TEABLE_SECTIONS)
-transactions_repo = None # Lazy load if needed or instantiate here
+transactions_repo = None  # Lazy load if needed or instantiate here
 # Wait, I need TransactionsRepo for BudgetService
 from repositories.transactions import TransactionsRepository
-transactions_repo = TransactionsRepository(teable_service, Config.TEABLE_TRANSACTIONS)
+
+transactions_repo = TransactionsRepository(teable_service,
+                                           Config.TEABLE_TRANSACTIONS)
 
 budget_service = BudgetService(budgets_repo, sections_repo, transactions_repo)
+
 
 @bp.route('/', methods=['GET'])
 def index() -> tuple[Response, int]:
     return jsonify(budgets_repo.all()), 200
 
+
 @bp.route('/active', methods=['GET'])
 def active() -> tuple[Response, int]:
     return jsonify(budgets_repo.get_by_status('Active')), 200
 
-@bp.route('/<string:budget_id>/sections', methods=['GET'])
-def sections(budget_id: str) -> tuple[Response, int]:
-    return jsonify(sections_repo.filter_by_budget(budget_id)), 200
 
 @bp.route('/inactive', methods=['GET'])
 def inactive() -> tuple[Response, int]:
     return jsonify(budgets_repo.get_by_status('Inactive')), 200
+
 
 @bp.route('/<string:id>', methods=['GET'])
 def get_by_id(id: str) -> tuple[Response, int]:
--- src/static/scripts/budgets/details.js
+++ src/static/scripts/budgets/details.js
@@ -4,15 +4,20 @@
  */
 
 class BudgetDetailsManager {
-    constructor() {
+    bootstrap(){
         this.budgetId = this.getBudgetIdFromUrl();
         this.budgetTitle = document.getElementById('budgetTitle');
         this.projectionValue = document.getElementById('projectionValue');
+        this.remainingValue = document.getElementById('remainingValue');
         this.availableValue = document.getElementById('availableValue');
-        this.categoriesAccordion = document.getElementById('categoriesAccordion');
-        this.categoriesLoader = document.getElementById('categoriesLoader');
+        this.sectionsAccordion = document.getElementById('categoriesAccordion');
+        this.sectionsLoader = document.getElementById('categoriesLoader');
         
         this.init();
+    }
+    
+    constructor() {
+        this.bootstrap()
     }
 
     init() {
@@ -40,7 +45,7 @@ class BudgetDetailsManager {
                 this.fetchBudgetDetails();
             } else {
                 this.budgetTitle.textContent = "No Active Budget";
-                this.categoriesLoader.innerHTML = '<p class="text-muted">Please create a budget first.</p>';
+                this.sectionsLoader.innerHTML = '<p class="text-muted">Please create a budget first.</p>';
             }
         } catch (error) {
             console.error('Error fetching active budget:', error);
@@ -49,58 +54,56 @@ class BudgetDetailsManager {
 
     async fetchBudgetDetails() {
         try {
-            const [budgetRes, sectionsRes] = await Promise.all([
-                fetch(`/api/budgets/${this.budgetId}`),
-                fetch(`/api/budgets/${this.budgetId}/sections`)
+            const [budgetRes] = await Promise.all([
+                fetch(`/api/budgets/${this.budgetId}`)
             ]);
 
-            if (!budgetRes.ok || !sectionsRes.ok) throw new Error('Failed to fetch budget details');
+            if (!budgetRes.ok ) throw new Error('Failed to fetch budget details');
 
             this.budget = await budgetRes.json();
-            this.sections = await sectionsRes.json();
 
-            this.renderDetails(this.budget, this.sections);
+            this.renderBudgets(this.budget);
+            this.renderSections(this.budget.fields.sections);
+            const sectionProjection = calculateSectionProjection()
+            this.remainingValue.textContent = this.budget.fields.projection - sectionProjection
+
         } catch (error) {
             console.error('Error fetching budget details:', error);
             this.renderError('Failed to load budget details.');
         }
     }
 
-    renderDetails(budget, sections) {
-        let fields = budget[0].fields;
-        this.budgetTitle.textContent = fields.name || 'Budget Details';
-        this.projectionValue.textContent = fields.projection || '0';
-        this.availableValue.textContent = fields.available || '0';
-        
-        this.renderSections(sections);
+    renderBudgets(budget) {
+        this.budgetTitle.textContent = budget.fields.name || 'Budget Details';
+        this.projectionValue.textContent = budget.fields.projection || '0';
+        this.remainingValue.textContent = '0';
     }
 
     renderSections(sections) {
-        this.categoriesLoader.classList.add('d-none');
+        this.sectionsLoader.classList.add('d-none');
         
         if (sections.length === 0) {
-            this.categoriesAccordion.innerHTML = '<div class="text-center py-4 text-muted">No sections found for this budget.</div>';
+            this.sectionsAccordion.innerHTML = '<div class="text-center py-4 text-muted">No sections found for this budget.</div>';
             return;
         }
 
-        this.categoriesAccordion.innerHTML = sections.map((sec, index) => this.createSectionAccordionItem(sec, index)).join('');
+        this.sectionsAccordion.innerHTML = sections.map((sec, index) => this.createSectionAccordionItem(sec, index)).join('');
     }
 
     createSectionAccordionItem(section, index) {
-        let fields = section.fields;
-        let entries = []
+        const remaining = calculateRemaining(section)
         const collapseId = `categoryCollapse${index}`;
         
-        return `
+        const acordionItems = `
             <div class="card mb-3 border-0 shadow-sm">
                 <div class="card-header bg-white border-0 py-3">
                     <div class="d-flex align-items-center justify-content-between">
                         <button class="btn btn-link text-dark text-decoration-none p-0 d-flex align-items-center gap-3 accordion-toggle collapsed"
                             data-bs-toggle="collapse" data-bs-target="#${collapseId}">
                             <i class="bi bi-chevron-down accordion-chevron"></i>
-                            <span class="fw-medium">${fields.label}</span>
-                            <span class="text-muted ms-3">${fields.projection || 0}</span>
-                            <span class="text-muted ms-3">${fields.projection || 0}</span>
+                            <span class="fw-medium">${section.fields.label}</span>
+                            <span class="text-muted ms-3 projection">${section.fields.projection || 0}</span>
+                            <span class="text-muted ms-3 remaining">${remaining || 0}</span>
                         </button>
                         <button class="btn btn-outline-secondary btn-sm rounded-pill px-3 border-0">
                             <i class="bi bi-pencil me-1"></i> Edit
@@ -119,12 +122,12 @@ class BudgetDetailsManager {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    ${entries.length > 0 
-                                        ? entries.map(e => `
+                                    ${section.fields.transactions.length > 0 
+                                        ? section.fields.transactions.map(transaction => `
                                             <tr>
-                                                <td class="text-muted border-end py-3">${e.date}</td>
-                                                <td class="text-dark fw-bold border-end py-3">${e.amount}</td>
-                                                <td class="text-muted py-3">${e.description}</td>
+                                                <td class="text-muted border-end py-3">${transaction.fields.date}</td>
+                                                <td class="text-dark fw-bold border-end py-3">${transaction.fields.amount}</td>
+                                                <td class="text-muted py-3">${transaction.fields.commerce}</td>
                                             </tr>
                                         `).join('')
                                         : '<tr><td colspan="3" class="text-center py-3 text-muted">No entries</td></tr>'
@@ -136,9 +139,45 @@ class BudgetDetailsManager {
                 </div>
             </div>
         `;
+        return acordionItems
     }
 }
 
+function calculateRemaining(section) {
+    let totalSection = 0.0;
+
+    const transactions = section.fields.transactions || [];
+    for (const transaction of transactions) {
+        totalSection += parseFloat(transaction.fields.amount || 0);
+    }
+
+    return parseFloat(section?.fields?.projection || 0) - totalSection;
+}
+
+function calculateSectionProjection(){
+    let sectionsProjection = 0 
+
+    const spans = document.querySelectorAll('span.projection');
+    for (const span of spans){
+        sectionsProjection = sectionsProjection + parseFloat(span.innerText)
+    }
+    return sectionsProjection
+}
+
+const budgetDetailsManaget = new BudgetDetailsManager();
+
 document.addEventListener('DOMContentLoaded', () => {
-    new BudgetDetailsManager();
+    budgetDetailsManaget.bootstrap()
+
+    // Select the button by its ID
+    const refreshButton = document.getElementById('refreshSections');
+
+    // Check if the button exists to avoid errors
+    if (refreshButton) {
+        refreshButton.addEventListener('click', function(event) {
+            console.log('Refresh Sections button clicked!');
+            budgetDetailsManaget.bootstrap()
+        });
+    }
 });
+
